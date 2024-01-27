acf75fa81c2ba92b4298283ba6243e9ecb8786a3
Kristina Nikiforova <kristina.nik93@gmail.com>
2024-01-27 19:22:48 +0200

    Added a factory for the service to choose with which provider to work with

--- bin/build.php
+++ bin/build.php
@@ -3,8 +3,11 @@
 use DI\ContainerBuilder;
 use GuzzleHttp\Client;
 use MapsTask\Controllers\GeocodingController;
+use MapsTask\Factories\GeocodingServiceFactory;
+use MapsTask\Mappers\GoogleMapsMapper;
 use MapsTask\Mappers\OSMGeocodingMapper;
 use MapsTask\Providers\GeocodingProviderInterface;
+use MapsTask\Providers\GoogleMapsProvider;
 use MapsTask\Providers\OSMGeocodingProvider;
 use MapsTask\Services\GeocodingService;
 
@@ -16,32 +19,40 @@ function build()
 	$containerBuilder = new ContainerBuilder();
 	$container = $containerBuilder->build();
 
-
-	// Register dependencies
+	// Register GuzzleHTTP Client
 	$container->set(Client::class, function () {
 		return new GuzzleHttp\Client();
 	});
 
-	$container->set(GeocodingProviderInterface::class,
-		\DI\create(OSMGeocodingProvider::class)
-			->constructor(
-				\DI\get(Client::class),
-				\DI\get(OSMGeocodingMapper::class)
-			)
+	// Register OSMGeocodingProvider
+	$container->set(OSMGeocodingProvider::class, \DI\create()
+		->constructor(
+			\DI\get(Client::class),
+			\DI\get(OSMGeocodingMapper::class)
+		)
 	);
-	//  $container->set(GeocodingProviderInterface::class,
-	//    \DI\create(GoogleMapsProvider::class)
-	//      ->constructor(
-	//        \DI\value('test_key'), //API key
-	//        \DI\get(Client::class),
-	//		\DI\get(GoogleMapsMapper::class)
-	//      )
-	//  );
-	$container->set(GeocodingService::class,
-		\DI\create()->constructor(\DI\get(GeocodingProviderInterface::class))
+
+	// Register GoogleMapsProvider
+	$container->set(GoogleMapsProvider::class, \DI\create()
+		->constructor(
+			\DI\value('test_key'), // API key
+			\DI\get(Client::class),
+			\DI\get(GoogleMapsMapper::class)
+		)
 	);
-	$container->set(GeocodingController::class,
-		\DI\create()->constructor(\DI\get(GeocodingService::class))
+
+	// Register GeocodingService
+	$container->set(GeocodingService::class, function () use ($container) {
+		$osmProvider = $container->get(OSMGeocodingProvider::class);
+		$googleMapsProvider = $container->get(GoogleMapsProvider::class);
+
+		// Choose to work with which provider
+		return GeocodingServiceFactory::create($osmProvider);
+	});
+
+	// Register GeocodingController
+	$container->set(GeocodingController::class, \DI\create()
+		->constructor(\DI\get(GeocodingService::class))
 	);
 
 	return $container;
