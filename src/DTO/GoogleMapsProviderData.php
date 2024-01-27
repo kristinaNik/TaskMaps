64da5eb8a38128af1a8f278b3c691c3726e00e37
Kristina Nikiforova <kristina.nik93@gmail.com>
2024-01-27 13:49:06 +0200

    Adding first test

--- src/DTO/GoogleMapsProviderData.php
+++ src/DTO/GoogleMapsProviderData.php
@@ -28,6 +28,4 @@ class GoogleMapsProviderData
 	{
 		return $this->longitude;
 	}
-
-
 }
---
+++ tests/MyTest.php
@@ -0,0 +1,45 @@
+<?php
+
+use MapsTask\DTO\OSMGeocodingProviderData;
+use MapsTask\Providers\GeocodingProviderInterface;
+use MapsTask\Services\GeocodingService;
+use PHPUnit\Framework\TestCase;
+
+class MyTest extends TestCase
+{
+
+	public function testGetCoordinatesFromAddressWithOSMData()
+	{
+		// Mock the GeocodingProviderInterface for OSM data
+		$osmProviderMock = $this->createMock(GeocodingProviderInterface::class);
+		$osmProviderMock->method('getData')->willReturn([
+			new OSMGeocodingProviderData(
+						123,
+				'test',
+				345,
+				      '10.123',
+				'20.345',
+				'class',
+				'type',
+				'na',
+				'addresstype',
+				'Varna',
+				'Varna, Bulgaria',
+			)
+		]);
+
+		// Create GeocodingService instance with the mock
+		$geocodingService = new GeocodingService($osmProviderMock);
+
+		// Call the method under test
+		$result = $geocodingService->getCoordinatesFromAddress('Varna');
+
+		// Assertions
+		$expectedResult = [
+			['name' => 'Varna', 'lon' => '20.345', 'lat' => '10.123']
+		];
+
+		$this->assertEquals($expectedResult, $result);
+	}
+
+}
